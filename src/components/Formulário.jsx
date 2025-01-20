aa3b99e4804bd1354aba7d286c7fbb55bdec3b94
joao <jaobgr77gg@gmail.com>
2025-01-20 10:52:52 -0300

    add-Number_form

--- src/components/Formulário.jsx
+++ src/components/Formulário.jsx
@@ -1,12 +1,16 @@
 import { useTranslation } from "react-i18next"
 import emailjs from "@emailjs/browser"
 import { useState, useEffect } from "react"
+import { parsePhoneNumber } from "awesome-phonenumber"
+
 import ButtonDes from "./buttonDes"
 export default function SimpleContactForm() {
     const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
     const [nEmpresa, setNEmpresa] = useState("")
     const [mensagem, setMensagem] = useState("")
+    const [number, setNumber] = useState(false)
+    const [string,setString] = useState("")
     const [verify, setVerify] = useState(true)
     const serviceId = "service_p0gm6hl"
     const templateId = "template_nfauqse"
@@ -14,28 +18,44 @@ export default function SimpleContactForm() {
         from_name: nome,
         email: email,
         empresa: nEmpresa,
-        message: mensagem
+        message: mensagem,
+        number: string,
     }
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     const publicKey = "z9CQ9sx93i6EfrFrA"
+
+    useEffect(() =>{
+        function Mask() {
+            let text = string 
+            text = (text || "").replace(/\D/g,"")
+            text = text.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+            setString(text)
+            console.log(number)
+            const pn = parsePhoneNumber(string,{regionCode:"BR"})
+            setNumber(pn.valid)
+            console.log(pn)
+        }
+        Mask()
+    },[string])
+
     function SendEmail() {
         emailjs.send(serviceId, templateId, templateParams, publicKey)
             .then(response => {
                 alert("Email enviado")
                 console.log("email send")
             })
-            .catch (err => console.log(err))
+            .catch(err => console.log(err))
     }
 
     useEffect(() => {
-        if (nome.trim().length > 0 && email.trim().length > 0 && nEmpresa.trim().length && mensagem.trim().length > 0) {
+        if (nome.trim().length > 0 && email.trim().length > 0 && nEmpresa.trim().length && mensagem.trim().length > 0  && number) {
             console.log("email valido")
             setVerify(false)
         } else {
             setVerify(true)
             console.log("email não valido")
         }
-    }, [nome, email, nEmpresa, mensagem])
+    }, [nome, email, nEmpresa, mensagem,number])
 
 
     return (
@@ -75,7 +95,8 @@ export default function SimpleContactForm() {
                                 />
                                 <label className="absolute top-2 left-2 text-gray-500 peer-valid:top-[-20px] peer-focus:top-[-20px] duration-150">{t("form_placeholder2")}</label>
                             </div>
-                            <div className="relative mb-10">
+
+                            <div className="relative mb-5">
                                 <input
                                     type="text"
                                     placeholder=""
@@ -87,11 +108,27 @@ export default function SimpleContactForm() {
                                 <label className="absolute top-2 left-2 text-gray-500 peer-valid:top-[-20px] peer-focus:top-[-20px] duration-150">{t("form_placeholder3")}</label>
                             </div>
                         </div>
-                        <div className="relative">
+
+                        <div className="relative ">
+                            <input
+                                type="text"
+                                placeholder=""
+                                className="peer w-full p-2 border-t-none border-b-2 bg-cinzaBg border-gray-300 outline-none mb-10"
+                                required
+                                value={string}
+                                max={10}
+                                onChange={(e) => { setString(e.target.value) }}
+                            />
+                            <label className="absolute top-2 left-2 text-gray-500 peer-valid:top-[-20px] peer-focus:top-[-20px] duration-150">{t("form_placeholder6")}</label>
+                        </div>
+
+                        <div className="relative mt-10">
                             <textarea
-                                className="peer w-full  border border-gray-300 bg-cinzaBg  h-8 outline-none border-x-0 border-t-0 p-2"
+                                className="peer w-full  border border-gray-300 bg-cinzaBg border-b-2  h-10 outline-none border-x-0 border-t-0 p-2"
                                 value={mensagem}
-                                onChange={(e) => { setMensagem(e.target.value) }}
+                                onChange={(e) => { 
+                                    setMensagem(e.target.value) 
+                                    }}
                                 required
                             ></textarea>
                             <label className="absolute top-[-2px] left-2 text-gray-500 peer-valid:top-[-20px] peer-focus:top-[-20px] duration-150">{t("form_placeholder4")}</label>
---
+++ src/components/InputMask.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect,useContext } from "react"
+import { parsePhoneNumber } from "awesome-phonenumber"
+import { NumberPhoneVerify } from "../Context/NumberPhoev";
+
+
+export default function InputPhoneMask() {
+    const [phoneNumber,setPhoneNumber,string,setString] = useContext (NumberPhoneVerify)
+    useEffect(() =>{
+        function Mask() {
+            let text = string 
+            text = (text || "").replace(/\D/g,"")
+            text = text.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+            setString(text)
+            console.log(phoneNumber)
+            const pn = parsePhoneNumber(string,{regionCode:"BR"})
+            setPhoneNumber(pn.valid)
+            console.log(pn)
+        }
+        Mask()
+    },[string])
+    return (
+        <>
+            <input className="w-full p-3 rounded-xl" type="text" placeholder="(00)00000-0000" maxLength={13} value={string} onChange={(e) => setString(e.target.value)} />
+            { !phoneNumber &&  string != "" ?
+                <p className="text-red-400 font-bold"> O número inserido não existe</p>
+            :null
+            }
+        </>
+    )
+}
